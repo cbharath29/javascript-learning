81a3aaf3602fc95bda1a671cd59ed0f18177d80b
Bharath <barathkumarc022@gmail.com>
2026-08-11 11:36:25 +0530

    Add variables example

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,16 @@
+// const variable cannot reassigned
+// const name = "bharath";
+// name = "kumar"; // This will throw an error because 'name' is a constant and cannot be reassigned.
+// console.log(name);
+
+
+// les variable can be change
+// let age = 22;
+// age = 23;
+// console.log(age);  // 23
+
+
+//var variable can be change
+let name = "Hyderabad";
+name =  "bangalore"; // This is valid because 'name' is declared with 'let' and can be reassigned.
+console.log(name);
