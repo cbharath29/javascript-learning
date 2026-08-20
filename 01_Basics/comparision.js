7465cd0110dfb23da4eb8a067a45df280bbdda70
Bharath <barathkumarc022@gmail.com>
2026-08-20 23:30:06 +0530

    Add comparison basics example

---
+++ 01_Basics/comparision.js
@@ -0,0 +1,21 @@
+// we often need to compare values of different data types for this we mainly use:
+// ==   → Loose Equality
+// ===  → Strict Equality
+// !=   → Loose Inequality
+// !==  → Strict Inequality
+
+// ==  Loose Equality
+
+// console.log(5 == "5")
+// console.log(true == 1);
+
+// ===  → Strict Equality
+// No automatic type conversion is performed.
+// console.log( 5 === 5);
+// console.log( 5 === "5");
+
+// !=   → Loose Inequality
+// console.log(5 != "5");
+
+// !==  → Strict Inequality
+// console.log(5 !== "5");
